3eae4244d41046a472ba6271237d64c1f9634a54
minhanghuang <job@minhang.me>
2023-01-02 14:56:14 +0800

    perf: update odrSpiral libs

--- include/opendrive-cpp/common/spiral/odrSpiral.h
+++ include/opendrive-cpp/common/spiral/odrSpiral.h
@@ -5,32 +5,25 @@
  *              in OpenDRIVE applications
  * ---------------------------------------------------
  *  first edit:	09.03.2010 by M. Dupuis @ VIRES GmbH
- *  last mod.:  09.03.2010 by M. Dupuis @ VIRES GmbH
- * ===================================================
-
-    Copyright 2010 VIRES Simulationstechnologie GmbH
-
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-        http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
+ *  last mod.:  10.03.2020 by N. Dillmann @ ASAM e.V.
+ * =================================================== 
+ * ASAM OpenDRIVE 1.6 Spiral example implementation
+ * 
+ * 
+ * (C) by ASAM e.V., 2020
+ * Any use is limited to the scope described in the license terms. 
+ * The license terms can be viewed at www.asam.net/license
  */
-
+ 
+ 
 /**
- * compute the actual "standard" spiral, starting with curvature 0
- * @param s      run-length along spiral
- * @param cDot   first derivative of curvature [1/m2]
- * @param x      resulting x-coordinate in spirals local co-ordinate system [m]
- * @param y      resulting y-coordinate in spirals local co-ordinate system [m]
- * @param t      tangent direction at s [rad]
- */
+* compute the actual "standard" spiral, starting with curvature 0
+* @param s      run-length along spiral
+* @param cDot   first derivative of curvature [1/m2]
+* @param x      resulting x-coordinate in spirals local co-ordinate system [m]
+* @param y      resulting y-coordinate in spirals local co-ordinate system [m]
+* @param t      tangent direction at s [rad]
+*/
 
-extern void odrSpiral(double s, double cDot, double* x, double* y, double* t);
+extern void odrSpiral( double s, double cDot, double *x, double *y, double *t );
 
