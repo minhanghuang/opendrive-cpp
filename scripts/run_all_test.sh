dec9df84f5d8ed554a94dfe9c9aa4115036ef36d
minhanghuang <job@minhang.me>
2023-02-18 14:09:40 +0800

    perf: update script

--- scripts/run_all_test.sh
+++ scripts/run_all_test.sh
@@ -6,6 +6,8 @@ export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:/opt/owner/lib
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/owner/lib
 
 function test() {
+  cmake -B build -DBUILD_SHARED_LIBS=ON -DBUILD_OPENDRIVECPP_TEST=ON -DCMAKE_BUILD_TYPE=Debug .
+  cmake --build build -j6
   local bin_path="$current_path/../build/tests/"
   for file in $bin_path/*
   do
